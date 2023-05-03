f6b2042da4d9d261c1f9ec819e1c6a1ca4356d23
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-03 11:14:39 -0600

    Day 30: Return temperature for any station and date.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -11,9 +13,10 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    # df = pd.read_csv("")
-    # temperature = df.station(date)
-    temperature = 23
+    # zfill adds zeros before number, which is what we need for the correct format
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
     return {"Station": station,
             "date": date,
             "temperature": temperature}
@@ -21,4 +24,4 @@ def about(station, date):
 
 if __name__ == "__main__":
     # Specifying the port allows multiple Flask apps to run at the same time.
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
