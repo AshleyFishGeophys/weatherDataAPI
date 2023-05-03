574df0e413c1c08ccc22e63d30bd5451f5dc53ff
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-03 12:05:50 -0600

    Day 31: Add url endpoint for yearly and all data

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ def about(station, date):
             "date": date,
             "temperature": temperature}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # df["Temp_C"] == df['   TG'] / 10
+    all_temps = df[["    DATE", '   TG']].to_dict(orient="records")
+    return {"Station": station,
+            "temperatures": all_temps}
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     # Specifying the port allows multiple Flask apps to run at the same time.
